4eba184a926f0ab32430f4a9ed3e3b2f4fae66b2
aeseb <sebastoledano@outlook.com>
2022-12-01 22:59:36 -0300

    modificado

--- SuperSimioProgramador.js
+++ SuperSimioProgramador.js
@@ -1,62 +1,43 @@
-const TecladoPc = require("./TecladoPc");
 
 class SuperSimioProgramador{
 
     //Estado/caracteristica/campo
     simioNombre = "por def";
-    albertConAuriculares = true;
-
-    //Comportmiento/metodo
-    /**
-     * 
-     * @returns el nombre del SuperSimioProgramador
-     */
-    nombreSimio(){
-        return this.simioNombre;
-    }
-    /**
-     * 
-     * @param {string} nombre del SuperSimioProgramador
-     */
+    albertNoOye = true;
+    gilbertNoOye = false;
+    
+   
+    
     simioTeLlamas(nombre){
         this.simioNombre = nombre;
+        
     }
 
-    /**
-     * Teclado que va a usarse por cada
-     * uno de los super simios.
-     * @param {TecladoPc} unTeclado que es usado
-     */
-    programaCon(unTeclado){
-        console.log(this.nombreSimio(),"usa el teclado",unTeclado.tecladoNombre());
-        console.log("porque prefiere");
-        unTeclado.cualidadTipeo();
+    quitarAuriculares(unSuperSimio){
+        unSuperSimio.simioSePregunta();
     }
     
-    /**
-     * Este metodo muestra en consola una
-     * interaccion entre el objeto albert
-     * y el objeto gilbert segun dos estados
-     * booleanos.
-     */
-    simiosInteractuando(){
-        console.log("Gilbert le pide a Albert");
-        console.log("que use otro teclado...");
-
-        if(this.albertConAuriculares){
-            console.log("Pero Albert no lo oye y")
-            console.log("Gilbert le lanza su teclado.")
+    simioSePregunta(){
+        console.log("\n¿" + this.simioNombre +  " me quitó los auriculares?");  
+    }
+    
+    albertTenesAuri(){
+        if (!this.albertNoOye) {   
+            console.log("si >:(");
         }
         else{
-            console.log("Albert responde: pues no hagas");
-            console.log("la indentacion con espacios... ");
-            console.log("y Gilbert le lanza su teclado.")
-        }
-    }
-    conAuriculares(noOye){
-        this.albertConAuriculares = noOye;
+            console.log("no <:)");  
+        }   
     }
 
+    gilbertTenesAuri(){
+        if (!this.gilbertNoOye) {   
+            console.log("si >:(");
+        }
+        else{
+            console.log("no <:D");
+        }
+    }
 }
 
 module.exports = SuperSimioProgramador;
--- app1.js
+++ app1.js
@@ -1,39 +1,14 @@
 const SuperSimioProgramador = require("./SuperSimioProgramador");
-const TecladoPc = require("./TecladoPc");
-
 
 const albert = new SuperSimioProgramador;
 const gilbert = new SuperSimioProgramador;
-const simios = new SuperSimioProgramador;
-const preferido = new TecladoPc;
-const nocturno = new TecladoPc;
 
-/* El estado true (o false) debe variar
- segun lo que se desee mostrar en consola
- mediante el metodo simiosInteractuando
- */
-//simios.conAuriculares(false);
-
-// Flujo de los teclados
-preferido.tuNombreEs("Preferido");
-nocturno.tuNombreEs("Nocturno");
-nocturno.sistemaMecanico(false);
-
-console.log("\n---------Super Simios---------\n");
-// Flujo de Albert
 albert.simioTeLlamas("Albert");
-albert.programaCon(preferido);
-
-console.log("-------");
-
-// Flujo de Gilbert
 gilbert.simioTeLlamas("Gilbert");
-gilbert.programaCon(nocturno);
 
-console.log("-------");
+albert.quitarAuriculares(gilbert);
+albert.albertTenesAuri();
+gilbert.quitarAuriculares(albert);
+gilbert.gilbertTenesAuri();
 
-/**
- * Flujo de interaccion entre el objeto
- * albert y el objeto gilbert.
- */
-simios.simiosInteractuando();
+
