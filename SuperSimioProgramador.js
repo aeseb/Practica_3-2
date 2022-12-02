2ad514f2147ca04674a2737166e4310205c192be
aeseb <sebastoledano@outlook.com>
2022-12-02 17:41:28 -0300

    modificado

--- SuperSimioProgramador.js
+++ SuperSimioProgramador.js
@@ -3,26 +3,39 @@ class SuperSimioProgramador{
 
     //Estado/caracteristica/campo
     simioNombre = "por def";
-    albertNoOye = true;
-    gilbertNoOye = false;
-    
+    //albertNoOye = true;
+    //gilbertNoOye = false;
+    simioNoOye = "por def";
+
    
-    
+    /**
+     * @param {String} nombre recibe Albert o Gilbert como parametro.
+     */
     simioTeLlamas(nombre){
-        this.simioNombre = nombre;
-        
+        this.simioNombre = nombre;  
     }
 
+    /**
+      * @param {String} unSuperSimio recibe un objeto Albert o
+      * un objeto Gilbert como parametro.
+      */
     quitarAuriculares(unSuperSimio){
-        unSuperSimio.simioSePregunta();
+        unSuperSimio.simioMolesto();
     }
     
-    simioSePregunta(){
+    /**
+     * En esta funcion un simio se pregunta si
+     * otro simio -que es mencionado con su nombre-, le quito los auriculares.
+     */
+    simioMolesto(){
         console.log("\n¿" + this.simioNombre +  " me quitó los auriculares?");  
     }
-    
-    albertTenesAuri(){
-        if (!this.albertNoOye) {   
+
+    /**
+     * Funcion que dice cual simio no lleva auriculares
+     */
+    tenesAuriculares(){
+        if (!this.simioNoOye) {   
             console.log("si >:(");
         }
         else{
@@ -30,13 +43,12 @@ class SuperSimioProgramador{
         }   
     }
 
-    gilbertTenesAuri(){
-        if (!this.gilbertNoOye) {   
-            console.log("si >:(");
-        }
-        else{
-            console.log("no <:D");
-        }
+    /**
+     * Esta funcion permite reconocer el estado de cada objeto simio.
+     * @param {boolean} noOye recibe true o false como parametro.
+     */
+    conAuriculares(noOye){
+        this.simioNoOye = noOye;
     }
 }
 
--- app1.js
+++ app1.js
@@ -1,14 +1,16 @@
 const SuperSimioProgramador = require("./SuperSimioProgramador");
-
+ 
 const albert = new SuperSimioProgramador;
 const gilbert = new SuperSimioProgramador;
+
 
 albert.simioTeLlamas("Albert");
 gilbert.simioTeLlamas("Gilbert");
 
 albert.quitarAuriculares(gilbert);
-albert.albertTenesAuri();
+albert.conAuriculares(true);
+albert.tenesAuriculares();
 gilbert.quitarAuriculares(albert);
-gilbert.gilbertTenesAuri();
-
+gilbert.conAuriculares(false);
+gilbert.tenesAuriculares();
 
